18ad92028bb12f4645ed4cb72b6b0c9faa962b9c
aozbudak <abdurrahimozbudak376@gmail.com>
2026-09-02 15:23:19 +0300

    Update admin layout to pass staff data to sidebar and header components for improved user context.

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -39,7 +39,7 @@
     <body class="min-h-screen bg-background font-sans text-foreground antialiased">
         <a href="#main" class="sr-only focus:not-sr-only focus:absolute focus:top-2 focus:left-2 focus:z-50 focus:bg-primary focus:px-4 focus:py-2 focus:text-primary-foreground">Skip to content</a>
 
-        <x-admin.sidebar :sections="$navSections" :home-route="$homeRoute" />
+        <x-admin.sidebar :sections="$navSections" :home-route="$homeRoute" :staff="$staff" />
 
         <div class="min-h-screen lg:pl-[var(--sidebar-width)]">
             <x-admin.header :staff="$staff" :breadcrumbs="$breadcrumbs" :notifications="$notifications" />
